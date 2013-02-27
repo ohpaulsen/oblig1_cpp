b991a464f455427a4c736d614c9407c52d853a71
ohpaulsen <ole@ohpaulsen.no>
2013-02-27 16:21:47 +0100

    la til ny klasse

---
+++ class_blackjackGame.cpp
@@ -0,0 +1,18 @@
+#include "class_blackjackGame.h"
+
+using namespace std;
+using namespace casino;
+
+blackjackGame::blackjackGame(gameType g,int player)
+{
+
+}
+
+blackJackGame::playRound()
+{
+}
+
+blackJackGame::start()
+{
+
+}
